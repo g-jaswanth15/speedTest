b0575d963440e34d32fff0c10c221a576b440762
g-jaswanth15 <83649057+g-jaswanth15@users.noreply.github.com>
2021-08-26 15:03:03 +0530

    Add files via upload

--- on2.js
+++ on2.js
@@ -20,17 +20,37 @@ document.getElementById('click').addEventListener('click',()=>{
         document.getElementById('min').innerHTML = timecountmin +':'
         timecountsec++
     },1000)
-
+ 
     fetch('https://litipsum.com/api/1')
     .then(res=> res.text())
     .then(data=>{
         var data2= document.getElementById('text').innerHTML = data  
+        var data2char =document.getElementById('text')
+        setInterval(()=>{
+            var data1 = document.getElementById('textarea').innerText
+            for(let i=0;i<data1.length;i++){
+                if(data1.charAt(i)==' ' || data2.charAt==' '){
+                    document.getElementById('check').style.display = 'none'
+                }
+                if(data1.charAt(i) !== data2.charAt(i)){
+                    document.getElementById('check').style.display = 'block'
+                    document.getElementById('check').innerText='Please check grammer and also spaces , commas etc..'
+                }
+                else{
+                    document.getElementById('check').style.display = 'none'
+                }
+                if(data1.charAt(i) === data2.charAt(i)){
+                    document.getElementById('check').style.display = 'none'
+                }
+            }
+        },100)
     })
 
 })
 document.getElementById('submit').addEventListener('click',()=>{
     var data2= document.getElementById('text').innerHTML
     var data1 = document.getElementById('textarea').innerText
+    
     if(data1.length == (data2.length) ){
         document.getElementById('whole').style.display = 'none'
         clearInterval(mode)
@@ -49,4 +69,8 @@ document.getElementById('clear').addEventListener('click',()=>{
 document.getElementById('reload').addEventListener('click',()=>{
     location.reload(true)
 })
-
+var char=0
+setInterval(()=>{
+    var data2= document.getElementById('text').innerHTML
+    var data1 = document.getElementById('textarea').innerText
+},100)
